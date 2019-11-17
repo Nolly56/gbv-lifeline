49873ed2f8d8e2ae74bbd0a1344c80d7d6ac1c2d
Tlangelani Mkhabela <Tlangelani@Afrigis.local>
2019-11-17 22:58:28 +0200

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,38 @@
 <template>
   <div id="app">
-    <nav
-      class="navbar fixed-top"
-      style="background-color: #005495;"
-    >
+    <nav class="navbar fixed-top" style="background-color: #005495;">
       <img
         src="@/assets/images/LifeLine.png"
         alt="Life Line Logo"
         style="width:45px;margin-left:30px;"
-      >
+      />
 
       <div class="row actions-nav mr-1">
-        <div class="col-10 text-right">
+        <div class="col-3 text-right">
           <button
             disabled
             class="btn btn-primary"
             style="background-color: #005495;border-color: #005495;"
           >
-            <i class="fas fa-headset fa-1x " />
+            <i class="fas fa-headset fa-1x" />
           </button>
+        </div>
+        <div class="col-7 text-right">
+          <div class="dropdown">
+            <button
+              class="btn btn-primary dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >DATA TABELS</button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" data-toggle="modal" data-target="#saModal">Sexual Assault</a>
+              <a class="dropdown-item" data-toggle="modal" data-target="#aModal">Abductions</a>
+              <a class="dropdown-item" data-toggle="modal" data-target="#rModal">Rape</a>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
@@ -39,23 +53,72 @@
         </div>
       </div>
     </footer>
+
+<div class="modal fade" id="saModal" tabindex="-1" role="dialog">
+  <div class="modal-dialog modal-xl" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="saModalLabel">Sexual Assault</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Sexual Assault
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="aModal" tabindex="-1" role="dialog">
+  <div class="modal-dialog modal-xl" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="saModalLabel">Abductions</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+       Abductions
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="modal fade" id="rModal" tabindex="-1" role="dialog">
+  <div class="modal-dialog modal-xl" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="saModalLabel">Rape</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Rape
+      </div>
+    </div>
+  </div>
+</div>
   </div>
 </template>
 
 <script>
-import Landing from '@/views/Landing'
+import Landing from "@/views/Landing";
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Landing
   },
-  data () {
+  data() {
     return {
       year: new Date().getFullYear(),
       appVersion: process.env.VUE_APP_VERSION
-    }
+    };
   }
-}
+};
 </script>
 
 <style lang="scss">
--- src/components/SearchComponents/Address.vue
+++ src/components/SearchComponents/Address.vue
@@ -64,7 +64,4 @@ export default {
   max-width: 600px;
   max-height: 45px;
 }
-#Address{
-
-}
 </style>
